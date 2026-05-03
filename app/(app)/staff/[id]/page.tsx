4044dfda8c2c24f99012a8504313d8772e3e8587
Tobias <samueltobiasrolanduyet@gmail.com>
2026-05-03 18:46:17 +0200

    staff slug page missed padding, now added

--- app/(app)/staff/[id]/page.tsx
+++ app/(app)/staff/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function EditStaffPage({
     .where(eq(staffAvailability.staffId, id))
 
   return (
-    <div className="grid gap-6">
+    <div className="grid gap-6 p-6">
       <h1 className="text-2xl font-semibold">{row.name}</h1>
 
       <Card>
