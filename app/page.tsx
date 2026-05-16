4af19f8a1fd8f683129b62dfb1ceb7b356a7691f
Tobias <samueltobiasrolanduyet@gmail.com>
2026-05-16 22:39:39 +0200

    huskey should not pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 
 export default function Page() {
+  const test = 0
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
